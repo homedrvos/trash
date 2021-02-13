31112d7e188ab04a11ae906cbbc15f7e24eb210c
Lonnie Liu <h8liu@shanhu.io>
2021-02-13 01:23:10 +0000

    fix tests

--- conf/conf_test.go
+++ conf/conf_test.go
@@ -5,41 +5,52 @@ import (
 )
 
 func TestDuplicates(t *testing.T) {
+	newImport := func(pkg, version, repo string) Import {
+		return Import{Package: pkg, Version: version, Repo: repo}
+	}
 
 	testData := []struct {
 		imports    []Import
 		duplicates int
 	}{
 		{[]Import{
-			{"package1", "version1", ""},
+			newImport("package1", "version1", ""),
 		}, 0},
 		{[]Import{
-			{"package1", "version1", ""},
-			{"package2", "version1", "repoA"},
+			newImport("package1", "version1", ""),
+			newImport("package2", "version1", "repoA"),
 		}, 0},
 		{[]Import{
-			{"package1", "version1", ""},
-			{"package2", "version1", "repoA"},
-			{"package1", "version1", ""},
+			newImport("package1", "version1", ""),
+			newImport("package2", "version1", "repoA"),
+			newImport("package1", "version1", ""),
 		}, 1},
 		{[]Import{
-			{"package1", "version1", ""},
-			{"package2", "version1", "repoA"},
-			{"package1", "version1", ""},
-			{"package1", "version1", ""},
+			newImport("package1", "version1", ""),
+			newImport("package2", "version1", "repoA"),
+			newImport("package1", "version1", ""),
+			newImport("package1", "version1", ""),
 		}, 2},
 		{[]Import{
-			{"package1", "version1", ""},
-			{"package2", "version1", "repoA"},
-			{"package1", "version1", ""},
-			{"package1", "version1", ""},
-			{"package2", "version2", "repoB"},
-			{"package3", "version1", "repoA"},
+			newImport("package1", "version1", ""),
+			newImport("package2", "version1", "repoA"),
+			newImport("package1", "version1", ""),
+			newImport("package1", "version1", ""),
+			newImport("package2", "version2", "repoB"),
+			newImport("package3", "version1", "repoA"),
 		}, 3},
 	}
 
 	for i, d := range testData {
-		trash := Conf{"", d.imports, nil, nil, "", false}
+		trash := Conf{
+			Package:   "",
+			Imports:   d.imports,
+			Excludes:  nil,
+			Packages:  nil,
+			ImportMap: nil,
+			confFile:  "",
+			yamlType:  false,
+		}
 		trash.Dedupe()
 
 		if d.duplicates != len(d.imports)-len(trash.Imports) {
--- trash.go
+++ trash.go
@@ -19,8 +19,8 @@ import (
 	"github.com/urfave/cli"
 
 	"github.com/Masterminds/glide/godep"
-	"github.com/rancher/trash/conf"
-	"github.com/rancher/trash/util"
+	"github.com/homedrvos/trash/conf"
+	"github.com/homedrvos/trash/util"
 	"gopkg.in/yaml.v2"
 )
 
